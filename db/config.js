2b5820fba42c10093a49f7ebca85c37e6ddf0144
isai <isaiorellana.dev@gmail.com>
2022-03-02 11:22:00 -0600

    apply dbUrl

--- db/config.js
+++ db/config.js
@@ -1,16 +1,17 @@
-const { config } = require('../config/config');
-
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const { config } = require('./../config/config');
 
 module.exports = {
   development: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres',
   },
   production: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
